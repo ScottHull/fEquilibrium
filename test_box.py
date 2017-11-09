d20bd23446ebcb03be08029a3877362f5393e2db
ScottHull <scottdhull21@gmail.com>
2017-11-09 12:34:51 -0500

    Replaced index iteration with tuple iteration.  Much faster.

--- test_box.py
+++ test_box.py
@@ -4,13 +4,13 @@ import os
 
 
 # instantiate the box
-model = box(length=10, width=10, height=10, model_time=100, space_resolution=1, visualize_system=True, object_history=True)
+model = box(length=3, width=3, height=10, model_time=10, space_resolution=1, visualize_system=True, object_history=True)
 
 # insert one more more matrix materials into the box, will populate all possible coordinate positions
 model.insert_matrix(matrix_material='Silicate Liquid', composition={'SiO2': 50, 'FeO': 50, '182-Hf': 100},
-                    z_range=[0,1500], initial_temperature=2000)
+                    z_range=[0,5], initial_temperature=2000)
 model.insert_matrix(matrix_material='Denser Silicate Liquid', composition={'SiO2': 50, 'FeO': 50, '182-Hf': 100},
-                    z_range=[1500,3000], initial_temperature=2000)
+                    z_range=[5,10], initial_temperature=2000)
 
 # insert X number of objects into the box, specify their location, will overwrite matrix at that point
 for i in list(range(1)):
